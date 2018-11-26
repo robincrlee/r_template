f23169f1d198ea3e35b24bcd5fb93506a68ec690
robinlee <robin.lee@nytimes.com>
2018-11-26 15:19:24 -0500

    add comments

--- r_bigquery.R
+++ r_bigquery.R
@@ -6,14 +6,20 @@ library(dplyr)
 library(purrr)
 library(tidyr)
 
+## Set up connection to BigQuery
 conn <- dbConnect(bigquery(), project = "", dataset = "", 
                   use_legacy_sql = FALSE)
 
+## I often would modify SQL query by a little bit
+## In these situations, I would create a function to replace a small portion so that I can re-use most of my queries
 create_sql <- function(metric){
+	## paste function concatenates strings in R
 	sql <- paste("select ... from ...", " where type = '", metric, "' group by 1")
 	return(sql)
 } 
 
+
+## Query data using one query and plot the results. 
 sql_metric1 <- create_sql(metric1)
 df_metric1 <- dbGetQuery(conn,sql_metric1)
 df_metric1_clean <- df_metric1 %>% mutate() %>% filter()
@@ -21,6 +27,8 @@ df_metric1_clean <- df_metric1 %>% mutate() %>% filter()
 ggplot(df_metric1_clean , aes(var1)) +
   geom_histogram()+
   facet_wrap(vars(var2), scales = "free")
+
+## Query data using a slightly modified query and plot the results. 
 
 sql_metric2 <- create_sql(metric2)
 df_metric2 <- dbGetQuery(conn,sql_metric1)
